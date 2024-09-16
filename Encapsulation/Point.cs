8927446b37d91c7c75bcae37102758b129a7e29b
Nazarets Ksenia <ksenianazarec995@gmail.com>
2024-09-16 06:58:42 -0700

    Add project files.

---
+++ Encapsulation/Point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Point
+    {
+        double x;
+        double y;
+        public double X { get;set; }
+        public double Y { get;set; }   //auto - features
+
+        public double Distance(Point A)
+        {
+            return Math.Sqrt(Math.Pow(X - A.X,2) + Math.Pow(Y - A.Y, 2));
+        }
+
+
+        //CONSTRUCTOR
+        public Point(double x=0,double y = 0)
+        {
+            X = x;
+            Y = y;
+            Console.WriteLine($"constructor:\t {GetHashCode()}");
+        }
+
+        ~Point()
+        {
+            Console.WriteLine($"Destructor:\t {GetHashCode()}");
+        }
+        // Methods
+        public void Print()
+        {
+            Console.WriteLine($"X = {X} , Y = {Y}");      
+        }
+    }
+
+}
+
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static double Distance(Point A, Point B)
+        {
+          // return A.Distance(B);
+           return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+        }
+        static void Main(string[] args)
+        {
+            Point A = new Point(); // new call Constructor of class;
+            A.Print();
+
+
+            Point B = new Point(11,12); //default 
+            //ref       //object
+            B.X = 3;
+            B.Y = 4;
+            B.Print();
+            Console.WriteLine(B.Distance(A));
+            Console.WriteLine(A.Distance(B));
+            Console.WriteLine(Distance(A,B));
+            Console.WriteLine(Distance(B,A));
+
+        }
+    }
+}
